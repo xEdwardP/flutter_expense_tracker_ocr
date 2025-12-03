91cdd825b26e7250dc4466d795889e24530a3bf9
JoseAlvarado <boanerges5orellana@gmail.com>
2025-12-02 19:07:57 -0600

    Delete Credential

--- lib/data/firebase_config_example.dart
+++ lib/data/firebase_config_example.dart
@@ -1,13 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 
 const firebaseConfig = FirebaseOptions(
-  apiKey: "AIzaSyCPFVJStuovsN3_-cCmNZBfULxSxB92E4g",
-  authDomain: "spendwise-3154f.firebaseapp.com",
-  projectId: "spendwise-3154f",
-  storageBucket: "spendwise-3154f.firebasestorage.app",
-  messagingSenderId: "49973677896",
-  appId: "1:49973677896:web:50fe554ac3098e76242d7d",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 );
 
 const webClientToken =
-    "49973677896-kchr86598hecp949iqq2gemb18ucsr4j.apps.googleusercontent.com";
+    "";
